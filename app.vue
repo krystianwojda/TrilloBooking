030f7b666d7b88e392b53077834eac834ddd9c17
Krystian <krystian.wojda-98@wp.pl>
2023-10-04 14:54:56 +0200

    Building the Header

--- app.vue
+++ app.vue
@@ -1,9 +1,3 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
-</template>
-
 <script setup>
 useHead({
   link: [
@@ -16,3 +10,76 @@ useHead({
   ]
 });
 </script>
+
+<template>
+  <!--HEADER-->
+  <header class="header">
+    <img class="header__logo" src="@/public/logo.png" alt="logo">
+    <!--USER NAVIGATION-->
+    <nav class="user-nav">
+      <div class="user-nav__item">
+        <span class="user-nav__item-title">Login</span>
+        <img class="user-nav__item-icon" src="@/public/login.png" alt="Login">
+      </div>
+      <div class="user-nav__item">
+        <span class="user-nav__item-title">Register</span>
+        <img class="user-nav__item-icon" src="@/public/new-user.png" alt="Register">
+      </div>
+    </nav>
+    <!--USER NAVIGATION-->
+  </header>
+  <!--HEADER-->
+</template>
+
+<style lang="scss">
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+.header {
+  font-size: 1.4rem;
+  height: 7rem;
+  background-color: #fff;
+  border-bottom: 1px solid #f4f2f2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &__logo {
+    height: 3.25rem;
+    margin-left: 2rem;
+  }
+
+  .user-nav {
+    display: flex;
+    align-items: center;
+    align-self: stretch;
+    height: 7rem;
+    color: #777;
+
+    & > * {
+      padding: 0 2rem;
+      cursor: pointer;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      position: relative;
+    }
+
+    & > *:hover {
+      background-color: #F4F2F2FF;
+    }
+
+    &__item-title {
+      font-size: 1.8rem;
+      margin-right: 1rem;
+    }
+
+    &__item-icon {
+      width: 3rem;
+      height: 3rem;
+    }
+  }
+
+}
+</style>
