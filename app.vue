1dcd6af81874abc059cafa0467b568a568560e62
Krystian <krystian.wojda-98@wp.pl>
2023-10-05 11:33:44 +0200

    Building Search Bar

--- app.vue
+++ app.vue
@@ -15,6 +15,12 @@ useHead({
   <!--HEADER-->
   <header class="header">
     <img class="header__logo" src="@/src/logo.png" alt="logo">
+    <!--SEARCH BAR -->
+    <div class="header__search-bar">
+      <input type="text" class="header__search-bar__input" placeholder="Search your travel">
+      <button class="header__search-bar__button">Search</button>
+    </div>
+    <!--SEARCH BAR END -->
     <!--USER NAVIGATION-->
     <nav class="user-nav">
       <div class="user-nav__item">
@@ -22,13 +28,13 @@ useHead({
         <img class="user-nav__item-icon" src="@/src/login.png" alt="Login">
       </div>
       <div class="user-nav__item">
-        <span class="user-nav__item-title">Register</span>
+        <span class="user-nav__item-title">Registration</span>
         <img class="user-nav__item-icon" src="@/src/new-user.png" alt="Register">
       </div>
     </nav>
-    <!--USER NAVIGATION-->
+    <!--USER NAVIGATION END-->
   </header>
-  <!--HEADER-->
+  <!--HEADER END-->
   <!--HOME-->
   <section class="hero">
     <div class="hero__text-box">
@@ -40,7 +46,7 @@ useHead({
       </div>
     </div>
   </section>
-  <!--HOME-->
+  <!--HOME END-->
 </template>
 
 <style lang="scss">
@@ -67,6 +73,59 @@ body {
   &__logo {
     height: 3.25rem;
     margin-left: 2rem;
+  }
+
+  &__search-bar {
+    flex: 0 0 40%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media screen and (max-width: 1024px) {
+      flex: 0 0 50%;
+    }
+
+    &__input {
+      font-family: inherit;
+      font-size: inherit;
+      color: inherit;
+      background-color: $color-grey-light-2;
+      border: 1px solid $color-grey-light-4;
+      padding: .7rem 2rem;
+      width: 100%;
+      border-radius: 100px;
+      transition: all .7s;
+      margin-right: 2rem;
+
+      &:focus {
+        outline: none;
+        background-color: $color-grey-light-3;
+        transform: scale(1.03);
+      }
+
+      &::-webkit-input-placeholder {
+        font-weight: 100;
+        color: $color-grey-dark-3;
+      }
+    }
+
+    &__button {
+      padding: .7rem 3rem;
+      font-size: inherit;
+      color: #fff;
+      border: none;
+      font-weight: 300;
+      text-transform: uppercase;
+      border-radius: 100px;
+      background-image: linear-gradient(to right, $color-primary-light, $color-primary-dark);
+      position: relative;
+      transition: all .7s;
+    }
+
+    &__button:hover {
+      background-image: linear-gradient(to right, #ff094f, #850237);
+      transform: scale(1.05);
+    }
   }
 
   .user-nav {
