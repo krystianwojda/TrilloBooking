61a3d8d86788d7f4c2c22996e490c4715a2f1813
Krystian <krystian.wojda-98@wp.pl>
2023-10-06 10:10:58 +0200

    Create footer

--- app.vue
+++ app.vue
@@ -123,6 +123,36 @@ useHead({
     </div>
   </section>
   <!--POPULAR TRAVEL END -->
+  <!--FOOTER-->
+  <footer class="footer">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-12">
+          <div class="footer__logo">
+            <img class="footer__logo-img" src='@/src/logo.png' alt="Logo">
+            <span class="footer__name">TrilloBooking</span>
+          </div>
+        </div>
+        <div class="col-12 col-md-5">
+          <div class="footer__navigation">
+            <ul class="footer__navigation__list">
+              <li class="footer__navigation__list-item">company</li>
+              <li class="footer__navigation__list-item">contact us</li>
+              <li class="footer__navigation__list-item">carrers</li>
+              <li class="footer__navigation__list-item">privacy policy</li>
+              <li class="footer__navigation__list-item">terms</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-md-5">
+          <div class="footer__copyright">
+            <p>Build by <a class="footer__link" href="https://www.facebook.com/krystian.wojda.9/" target="_blank">KRYSTIAN WOJDA</a> Frontend Developer/Web Developer. Copyright © by Krystian Wojda. Designe this website created by Jonas Schmedtmann and Krystian Wojda.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--FOOTER END-->
 </template>
 
 <style lang="scss">
@@ -437,6 +467,72 @@ body {
 
   .card:hover {
     transform: scale(1.02);
+  }
+}
+
+.footer {
+  background-color: $color-grey-dark-1;
+  padding: 8rem 0;
+  color: #fff;
+
+  &__logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &-img {}
+  }
+
+  &__name {
+    font-size: 4rem;
+    margin-left: 2rem;
+  }
+
+  &__copyright {
+    padding-top: 8rem;
+    font-size: 1.4rem;
+
+    p {
+      border-top: $border-line;
+      padding-top: 2rem;
+    }
+  }
+
+  &__navigation {
+    padding-top: 8rem;
+    font-size: 1.6rem;
+
+    &__list {
+      list-style: none;
+      display: flex;
+      border-top: $border-line;
+      padding: 2rem 0 0 0;
+      text-transform: uppercase;
+
+      @media screen and (max-width: 1400px) {
+        flex-wrap: wrap;
+      }
+
+      &-item:not(:last-child) {
+        margin-right: 2rem;
+      }
+
+      &-item:hover {
+        color: $color-primary;
+        cursor: pointer;
+      }
+    }
+  }
+
+  &__link {
+    color: $color-primary;
+    text-decoration: none;
+    position: relative;
+    transition: all .7s;
+  }
+
+  &__link:hover {
+    color: $color-primary-light;
   }
 }
 </style>
