1e385fc7f62f5458deb1141189f83c317f96199c
Krystian <krystian.wojda-98@wp.pl>
2023-10-20 17:15:34 +0200

    Add protection in admin routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useSupabaseUser();
-    if (user.value) {
+    if (user.value.id === '260a3d29-4084-4b66-aa15-2417c7d68a67') {
         return;
     }
     return navigateTo('/login');
