a8673e200fc1e712debbfe3c3966ad4839509028
Krystian <krystian.wojda-98@wp.pl>
2023-10-24 10:35:28 +0200

    Extracting the Path Parameters

--- server/api/travels/[country].js
+++ server/api/travels/[country].js
@@ -1,5 +1,13 @@
-import country from '@/data/country.json';
+import travels from '@/data/travels.json';
 
 export default defineEventHandler((event) => {
-    return country;
+    const {country} = event.context.params;
+
+    let filteredTravels = travels;
+
+    filteredTravels = filteredTravels.filter(travel => {
+       return travel.country.toLowerCase() === country.toLowerCase()
+    });
+
+    return filteredTravels;
 });
