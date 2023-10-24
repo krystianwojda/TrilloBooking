d09241dd4e3894c803488ecdfaaf0bc69b6a40b6
Krystian <krystian.wojda-98@wp.pl>
2023-10-24 15:49:19 +0200

    Extracting the Query Parameters

--- server/api/travels/[country].js
+++ server/api/travels/[country].js
@@ -1,13 +1,24 @@
 import travels from '@/data/travels.json';
 
 export default defineEventHandler((event) => {
-    const {country} = event.context.params;
+    const { country } = event.context.params;
+    const { city } = getQuery(event);
 
     let filteredTravels = travels;
 
     filteredTravels = filteredTravels.filter(travel => {
        return travel.country.toLowerCase() === country.toLowerCase()
     });
+
+    if (country === 'All') {
+        return travels
+    }
+
+    if (city) {
+        filteredTravels = filteredTravels.filter(travel => {
+            return travel.city.toLowerCase() === city.toLowerCase();
+        })
+    }
 
     return filteredTravels;
 });
